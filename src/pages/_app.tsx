8ce3096459a1d1556c651aa10cbbdcdf57916fa7
seymennklc <seymennklc@gmail.com>
2023-01-15 19:07:20 +0300

    chore: import tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from '@/config/theme';
 import '@/styles/globals.css';
-import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </ChakraProvider >
   );
 }
